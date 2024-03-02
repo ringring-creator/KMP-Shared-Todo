9425641346b43ac78804d41c46bd23ed1de2cbfb
ringring <ringring.creator@gmail.com>
2024-03-02 12:14:59 +0900

    Fixed a bug where changes made on the editing screen were not reflected in the list.

--- shared/src/commonMain/kotlin/di/CommonModule.kt
+++ shared/src/commonMain/kotlin/di/CommonModule.kt
@@ -2,15 +2,14 @@ package di
 
 import data.TodoRepository
 import org.koin.core.module.Module
-import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 import ui.editTodo.EditTodoScreenModel
 import ui.todos.TodosScreenModel
 
 fun commonModule() = module {
     single<TodoRepository> { TodoRepository(sqlDriver = get()) }
-    singleOf(::TodosScreenModel)
-    single { EditTodoScreenModel(todoRepository = get(), id = it[0]) }
+    factory { TodosScreenModel(todoRepository = get()) }
+    factory { EditTodoScreenModel(todoRepository = get(), id = it[0]) }
 }
 
 expect fun platformModule(): Module
