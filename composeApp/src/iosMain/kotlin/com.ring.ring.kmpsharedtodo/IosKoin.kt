b54dff4c324a50e0861e6f2cee82e21adc2d7a27
ringring <ringring.creator@gmail.com>
2024-03-13 11:24:37 +0900

    Removed unnecessary iOS code in dependency injection

--- composeApp/src/iosMain/kotlin/com.ring.ring.kmpsharedtodo/IosKoin.kt
+++ composeApp/src/iosMain/kotlin/com.ring.ring.kmpsharedtodo/IosKoin.kt
@@ -1,18 +1,9 @@
 package com.ring.ring.kmpsharedtodo
 
 import com.ring.ring.kmpsharedtodo.di.initKoin
-import com.ring.ring.kmpsharedtodo.ui.editTodo.EditTodoScreenModel
-import com.ring.ring.kmpsharedtodo.ui.todos.TodosScreenModel
-import org.koin.core.Koin
 import org.koin.core.KoinApplication
 
 fun KoinApplication.Companion.start(logEnabled: Boolean): KoinApplication = initKoin(
     appDeclaration = { },
     logEnabled = logEnabled
 )
-
-val Koin.todosScreenModel: TodosScreenModel
-    get() = get()
-
-val Koin.editTodoScreenModel: EditTodoScreenModel
-    get() = get()
--- iosApp/iosApp/Koin.swift
+++ iosApp/iosApp/Koin.swift
@@ -15,23 +15,3 @@ extension KoinApplication {
         shared
     }
 }
-
-extension KoinApplication {
-    private static let keyPaths: [PartialKeyPath<Koin>] = [
-        \.todosScreenModel,
-        \.editTodoScreenModel
-    ]
-    
-    static func inject<T>() -> T {
-        shared.inject()
-    }
-    
-    func inject<T>() -> T {
-        for partialKeyPath in Self.keyPaths {
-            guard let keyPath = partialKeyPath as? KeyPath<Koin, T> else { continue }
-            return koin[keyPath: keyPath]
-        }
-        
-        fatalError("\(T.self) is not registered with KoinApplication")
-    }
-}
